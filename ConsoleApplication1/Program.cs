95506be99732684fba0d3660be8ba1b2f4966fb4
D48 <D48@D46-P15>
2017-03-31 09:29:50 +0200

    hugy

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Coucou");
+            Console.WriteLine("Hello");
+            String s = Console.ReadLine();
+            Console.WriteLine(s);
         }
     }
 }
